0f64590ddad7ff342a65391d95f69a03ef7458e4
dennisppaul <dennis.paul@hfk-bremen.de>
2019-06-07 16:15:26 +0200

    updated copyright notice

--- src/teilchen/behavior/Steering.java
+++ src/teilchen/behavior/Steering.java
@@ -2,8 +2,8 @@
 /*
  * Teilchen
  *
- * Copyright (C) 2015
- *
+ * Copyright (C) 2019
+*
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
