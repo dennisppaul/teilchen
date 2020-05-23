64cc9cb6eaccc5a51769cd23eec22c9cdc4aaef0
dennisppaul <dennis.paul@hfk-bremen.de>
2020-05-23 11:03:35 +0200

    added processing library information

--- dist/copy_readme.sh
+++ dist/copy_readme.sh
@@ -5,7 +5,9 @@ ROOT=$(pwd)
 
 SRC=$ROOT/../README.md
 SRC_LICENSE=$ROOT/../LICENSE
+LIB_PROPS=$ROOT/../lib/library.properties
 DST=$ROOT/../processing-library/$LIB_NAME
 
 cp "$SRC" "$DST"
 cp "$SRC_LICENSE" "$DST"
+cp "$LIB_PROPS" "$DST"
