397b97b47e25f48d5f6afaa6994691cf3031918f
dennisppaul <dennis.paul@hfk-bremen.de>
2024-05-08 07:15:56 +0200

    updated copyright

--- src/teilchen/util/CubicleWorldView.java
+++ src/teilchen/util/CubicleWorldView.java
@@ -2,7 +2,7 @@
  * Teilchen
  *
  * This file is part of the *teilchen* library (https://github.com/dennisppaul/teilchen).
- * Copyright (c) 2023 Dennis P Paul.
+ * Copyright (c) 2024 Dennis P Paul.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
